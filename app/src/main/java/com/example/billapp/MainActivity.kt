010bfcc82ca1dd247708c4241498a462035a3d86
weiweijason <wegos9012@gmail.com>
2024-07-30 13:23:29 +0800

    finished MainScreen.kt and AddItemScreen.kt update MainActivity.kt

--- app/src/main/java/com/example/billapp/MainActivity.kt
+++ app/src/main/java/com/example/billapp/MainActivity.kt
@@ -1,47 +1,21 @@
+// MainActivity.kt
 package com.example.billapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
 import com.example.billapp.ui.theme.BillAppTheme
+import com.example.myapplication.MainScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BillAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BillAppTheme {
-        Greeting("Android")
     }
 }
