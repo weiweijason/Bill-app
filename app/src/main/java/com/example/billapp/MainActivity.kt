28f774668d7ce63fff5a29e42b810f63342f5a27
weiweijason <wegos9012@gmail.com>
2024-08-01 10:44:54 +0800

    主頁面個人區塊修正 + 新增程式禁止旋轉

--- app/src/main/java/com/example/billapp/MainActivity.kt
+++ app/src/main/java/com/example/billapp/MainActivity.kt
@@ -1,6 +1,7 @@
 // MainActivity.kt
 package com.example.billapp
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,6 +12,8 @@ import com.example.billapp.MainScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 禁止螢幕旋轉
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContent {
             BillAppTheme {
                 MainScreen()
