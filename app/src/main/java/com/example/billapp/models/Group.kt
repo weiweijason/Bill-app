8795b51e25dc4eb9ae7da6cbaff9bffb5594c0d0
Jason Lin <jasonlin930309@gmail.com>
2024-08-16 22:33:02 +0800

    add FirebaseRepository

--- app/src/main/java/com/example/billapp/models/Group.kt
+++ app/src/main/java/com/example/billapp/models/Group.kt
@@ -1,8 +1,11 @@
 package com.example.billapp.models
 
+import android.os.Parcelable
+
 data class Group(
     val name: String,
     val image: String,
+    val createdBy: String,
     val usersID: List<GroupTransaction>,  // List of user id
     val transactions: List<GroupTransaction>,
     val deptRelations: List<DeptRelation>
