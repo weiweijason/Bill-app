d148411918a8ba1a573982e09d16fd0665738b0a
Jason Lin <jasonlin930309@gmail.com>
2024-08-17 00:02:53 +0800

    add parcelable in DeptRelation and Group

--- app/src/main/java/com/example/billapp/models/DeptRelation.kt
+++ app/src/main/java/com/example/billapp/models/DeptRelation.kt
@@ -1,7 +1,37 @@
 package com.example.billapp.models
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class DeptRelation(
     val from: String = "",
     val to: String = "",
     val amount : Double = 0.0
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readDouble()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(from)
+        parcel.writeString(to)
+        parcel.writeDouble(amount)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<DeptRelation> {
+        override fun createFromParcel(parcel: Parcel): DeptRelation {
+            return DeptRelation(parcel)
+        }
+
+        override fun newArray(size: Int): Array<DeptRelation?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/example/billapp/models/Group.kt
+++ app/src/main/java/com/example/billapp/models/Group.kt
@@ -1,5 +1,6 @@
 package com.example.billapp.models
 
+import android.os.Parcel
 import android.os.Parcelable
 
 data class Group(
@@ -9,4 +10,36 @@ data class Group(
     val usersID: List<GroupTransaction>,  // List of user id
     val transactions: List<GroupTransaction>,
     val deptRelations: List<DeptRelation>
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.createTypedArrayList(GroupTransaction)!!,
+        parcel.createTypedArrayList(GroupTransaction)!!,
+        parcel.createTypedArrayList(DeptRelation)!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(image)
+        parcel.writeString(createdBy)
+        parcel.writeTypedList(usersID)
+        parcel.writeTypedList(transactions)
+        parcel.writeTypedList(deptRelations)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Group> {
+        override fun createFromParcel(parcel: Parcel): Group {
+            return Group(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Group?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
